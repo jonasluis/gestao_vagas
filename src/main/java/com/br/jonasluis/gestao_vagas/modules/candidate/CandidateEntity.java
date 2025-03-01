f204b4ca1b4ef5e9d83f290c054a308931474c21
jonasluis <jonasluis66@gmail.com>
2025-03-01 09:09:07 -0300

    feat: criando tabela de candidato

--- src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,27 @@
 package com.br.jonasluis.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
     @NotBlank
     @Pattern(regexp = "^\\s+", message = "O campo [username] não deve conter espaço")
@@ -22,5 +32,8 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
