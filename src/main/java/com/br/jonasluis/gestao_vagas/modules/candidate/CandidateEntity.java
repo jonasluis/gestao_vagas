13d575157928ed2f99927b61f401f649924a25a8
jonasluis <jonasluis66@gmail.com>
2025-03-01 08:29:53 -0300

    feat: Formatando mensagem de validação

--- src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,7 @@
 package com.br.jonasluis.gestao_vagas.modules.candidate;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
@@ -12,11 +13,12 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+    @NotBlank
+    @Pattern(regexp = "^\\s+", message = "O campo [username] não deve conter espaço")
     private String username;
     @Email(message = "O campo [email] deve conter um email válido")
     private String email;
-    @Length(min = 8, max = 20)
+    @Length(min = 8, max = 20, message = "A senha deve conter entre (8) e (20) caracteres")
     private String password;
     private String description;
     private String curriculum;
