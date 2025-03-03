32921bf7040c85b9ea5ba6f8e8eec081d3e00819
jonasluis <jonasluis66@gmail.com>
2025-03-03 07:04:50 -0300

    feat: criando entidade Company

---
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,40 @@
+package com.br.jonasluis.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "company")
+public class CompanyEntity {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID Id;
+    
+    @NotBlank
+    @Pattern(regexp = "^[^\\s]+$", message = "O campo [username] não deve conter espaços")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um email válido")
+    private String email;
+
+    @Length(min = 8, max = 20, message = "A senha deve conter entre (8) e (20) caracteres")
+    private String password;
+    private String website;
+    private String name;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
