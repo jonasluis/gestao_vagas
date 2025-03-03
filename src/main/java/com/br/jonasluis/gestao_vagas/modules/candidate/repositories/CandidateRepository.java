cdbc4bb385d3bf3d6ec61e02500c2ea70294fe03
jonasluis <jonasluis66@gmail.com>
2025-03-03 06:23:26 -0300

    feat: validando usuário j;a existente

--- src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -3,7 +3,10 @@ package com.br.jonasluis.gestao_vagas.modules.candidate.repositories;
 import com.br.jonasluis.gestao_vagas.modules.candidate.CandidateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CandidateRepository extends JpaRepository <CandidateEntity, UUID> {
+
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
