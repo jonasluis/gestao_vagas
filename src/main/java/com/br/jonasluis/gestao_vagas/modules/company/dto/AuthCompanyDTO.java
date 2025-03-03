8b6b890369f4e5bdb61634403fae0a6cadd63c08
jonasluis <jonasluis66@gmail.com>
2025-03-03 11:31:44 -0300

    feat: criado authenticate e geração de token da company

---
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/company/dto/AuthCompanyDTO.java
@@ -0,0 +1,4 @@
+package com.br.jonasluis.gestao_vagas.modules.company.dto;
+
+public record AuthCompanyDTO(String password, String username) {
+}
--- src/main/java/com/br/jonasluis/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,8 +1,49 @@
 package com.br.jonasluis.gestao_vagas.modules.company.useCases;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.br.jonasluis.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.br.jonasluis.gestao_vagas.modules.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public  String execute(AuthCompanyDTO authCompanyDTO) {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.username()).orElseThrow(
+                () -> {
+                    throw new UsernameNotFoundException("Username/password incorrect");
+                });
+
+        // Verificar a senha são iguais
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.password(), company.getPassword());
+        //Se mão for igual -> erro
+        if(!passwordMatches){
+            throw new AuthenticationException("Invalid credentials") {
+            };
+        }
+        // Se for -> Gerar token
+        Algorithm algorithm  = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("jonas_company")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+
+        return token;
+
+    }
 
 }
