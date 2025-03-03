05fe9b4c8b0c2553c19df7bc6ba5d6fcbd5c16bb
jonasluis <jonasluis66@gmail.com>
2025-03-03 06:52:15 -0300

    feat: Use case\service

---
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,24 @@
+package com.br.jonasluis.gestao_vagas.modules.candidate.useCases;
+
+import com.br.jonasluis.gestao_vagas.exceptions.UserFoundException;
+import com.br.jonasluis.gestao_vagas.modules.candidate.CandidateEntity;
+import com.br.jonasluis.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity){
+
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(),
+                candidateEntity.getEmail()).ifPresent((user) ->{
+            throw new UserFoundException();
+        });
+
+        return this.candidateRepository.save(candidateEntity);
+    }
+}
