756fae65f002eb3216573e6426e36aa2d7813b92
jonasluis <jonasluis66@gmail.com>
2025-03-03 09:15:10 -0300

    fix: validação corrigida

--- src/main/java/com/br/jonasluis/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -2,6 +2,7 @@ package com.br.jonasluis.gestao_vagas.modules.company.controllers;
 
 import com.br.jonasluis.gestao_vagas.modules.company.entities.CompanyEntity;
 import com.br.jonasluis.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class CompanyController {
     private CreateCompanyUseCase createCompanyUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity) {
          try {
             var result = this.createCompanyUseCase.execute(companyEntity);
              return ResponseEntity.ok().body(result);
