48d6358d28522119f98cc6ab8f9ca42a9c206244
jonasluis <jonasluis66@gmail.com>
2025-03-04 15:25:21 -0300

    fix: Filtro do token candidato

--- src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/candidate")
 public class AuthCandidateController {
 
     @Autowired
     private AuthCandidateUseCase authCandidateUseCase;
 
-    @PostMapping("/candidate")
+    @PostMapping("/auth")
     public ResponseEntity<Object> auth(@Valid @RequestBody AuthCandidateRequestDTO authCandidateRequestDTO){
          try {
              var result = this.authCandidateUseCase.execute(authCandidateRequestDTO);
--- src/main/java/com/br/jonasluis/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/security/SecurityCandidateFilter.java
@@ -27,15 +27,17 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
-        if(header != null) {
-            var token = this.jwtCandidateProvider.validateToken(header);
-            if (token == null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
+        if (request.getRequestURI().startsWith("/candidate")){
+            if(header != null) {
+                var token = this.jwtCandidateProvider.validateToken(header);
+                if (token == null) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
+                request.setAttribute("candidate_id", token.getSubject());
             }
-            request.setAttribute("candidate_id", token.getSubject());
-
         }
+
         filterChain.doFilter(request, response);
 
     }
