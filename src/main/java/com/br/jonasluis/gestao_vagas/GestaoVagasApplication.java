0a35f116d71b89fdc09ccda379ddf567090cb28a
jonasluis <jonasluis66@gmail.com>
2025-07-10 06:22:30 -0300

    chore: Configurar o Swagger

--- src/main/java/com/br/jonasluis/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/GestaoVagasApplication.java
@@ -1,9 +1,19 @@
 package com.br.jonasluis.gestao_vagas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Gestao de Vagas",
+				description = "API Responsavel por Gestao de vagas",
+				version = "1"
+
+		)
+)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
