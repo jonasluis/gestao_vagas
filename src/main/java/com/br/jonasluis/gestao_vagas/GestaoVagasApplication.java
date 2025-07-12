e961bdae7e004fb8f00d57b49131e641b9a6df89
jonasluis <jonasluis66@gmail.com>
2025-07-12 08:37:53 -0300

    feat: Criar SwaggerConfig

--- src/main/java/com/br/jonasluis/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/GestaoVagasApplication.java
@@ -1,23 +1,9 @@
 package com.br.jonasluis.gestao_vagas;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Gestao de Vagas",
-				description = "API Responsavel por Gestao de vagas",
-				version = "1"
-
-		)
-)
-@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
