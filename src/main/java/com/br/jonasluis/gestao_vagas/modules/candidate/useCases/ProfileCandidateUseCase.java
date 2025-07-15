cdf08ad5926e9d077b13ae515be597b5051f2684
jonasluis <jonasluis66@gmail.com>
2025-07-15 19:58:43 -0300

    refactor: Refatorar nomes das classes

--- src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ src/main/java/com/br/jonasluis/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -1,9 +1,9 @@
 package com.br.jonasluis.gestao_vagas.modules.candidate.useCases;
 
+import com.br.jonasluis.gestao_vagas.exceptions.UserNotFoundException;
 import com.br.jonasluis.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import com.br.jonasluis.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -17,7 +17,7 @@ public class ProfileCandidateUseCase {
     public ProfileCandidateResponseDTO execute(UUID idCandidate){
         var candidate = this.candidateRepository.findById(idCandidate)
                 .orElseThrow(() ->{
-                    throw new UsernameNotFoundException("User not found");
+                    throw new UserNotFoundException();
                 });
         var candidateDTO = ProfileCandidateResponseDTO.builder()
                 .description(candidate.getDescription())
